44c1042bc340c44fa7b472841f1a10028a236e4d
win9 <krrr883@gmail.com>
2024-01-30 13:28:32 +0900

    [Refactor] controller 분리
    모든 url매핑이 한 controller에 들어가지 않도록 분리

---
+++ src/main/java/controller/BoardController.java
@@ -0,0 +1,62 @@
+package controller;
+
+import dto.ResourceDto;
+import model.Board;
+import model.Model;
+import model.User;
+import service.BoardService;
+import service.UserService;
+import util.ParseParams;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.BiFunction;
+
+public class BoardController implements Controller{
+    private final BoardService boardService = new BoardService();
+    private final UserService userService = new UserService();
+
+    private Map<String, BiFunction<String, Object, ResourceDto>> methodMap =  new HashMap<>();
+
+    {
+        methodMap.put("/write.html", this::processBoard);
+        methodMap.put("/write", this::generateBoardResource);
+        methodMap.put("/qna/show", this::generateBoardDetailResource);
+    }
+
+    public ResourceDto generateBoardDetailResource(String session, Object queryParams) {
+        if (session == null) {
+            return ResourceDto.of("/user/login.html", 302, false);
+        }
+        Board board = boardService.findBoard((ParseParams) queryParams);
+        Model.addAttribute("board", board);
+        return ResourceDto.of("/qna/show.html");
+    }
+    public ResourceDto generateBoardResource(String session, Object bodyData) {
+        if (session == null) {
+            return ResourceDto.of("/user/login.html", 302, false);
+        }
+        User user = userService.findUserWithSession(session);
+        boardService.createBoard(user, (ParseParams)bodyData);
+        return ResourceDto.of("/index.html", 302);
+    }
+
+    public ResourceDto processBoard(String session, Object path) {
+        if (session == null) {
+            return ResourceDto.of("/user/login.html", 302,false);
+        }
+        return ResourceDto.of((String) path);
+    }
+
+    public BiFunction<String, Object, ResourceDto> getCorrectMethod(String path) {
+        for (String key : methodMap.keySet()) {
+            if (path.contains(key) && path.contains("?")) {
+                return methodMap.get(key);
+            }
+            else if (path.matches(key)) {
+                return methodMap.get(key);
+            }
+        }
+        return null;
+    }
+}
--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.function.BiFunction;
 
-public class UserController {
+public class UserController implements Controller{
     private final UserService userService = new UserService();
     private final BoardService boardService = new BoardService();
     private Map<String, BiFunction<String, Object, ResourceDto>> methodMap =  new HashMap<>();
@@ -24,35 +24,8 @@ public class UserController {
         methodMap.put("/index.html", this::processIndexResource);
         methodMap.put("/user/form.html", this::process);
         methodMap.put("/user/login.html", this::process);
-        methodMap.put("/write.html", this::processBoard);
-        methodMap.put("/write", this::generateBoardResource);
         methodMap.put("/user/login", this::loginUserResource);
-        methodMap.put("/qna/show", this::generateBoardDetailResource);
         methodMap.put("/user/login_failed.html", this::process);
-    }
-
-    public ResourceDto generateBoardDetailResource(String session, Object queryParams) {
-        if (session == null) {
-            return ResourceDto.of("/user/login.html", 302, false);
-        }
-        Board board = boardService.findBoard((ParseParams) queryParams);
-        Model.addAttribute("board", board);
-        return ResourceDto.of("/qna/show.html");
-    }
-    public ResourceDto generateBoardResource(String session, Object bodyData) {
-        if (session == null) {
-            return ResourceDto.of("/user/login.html", 302, false);
-        }
-        User user = userService.findUserWithSession(session);
-        boardService.createBoard(user, (ParseParams)bodyData);
-        return ResourceDto.of("/index.html", 302);
-    }
-
-    public ResourceDto processBoard(String session, Object path) {
-        if (session == null) {
-            return ResourceDto.of("/user/login.html", 302,false);
-        }
-        return ResourceDto.of((String) path);
     }
 
     public ResourceDto generateUserListResource(String session, Object path) {
